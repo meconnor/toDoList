50580fba95771f154b7a4413c23e6c6ef21308d8
Connor <c.meriweather55@gmail.com>
2023-11-28 13:40:58 -0600

    created materialUI todolist w dummy data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
