123057e5aa2a08afed03f53128b7d9fa5a8b0a6b
Connor <c.meriweather55@gmail.com>
2023-11-28 14:37:44 -0600

    added toggle functionality to todoItem component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -17,6 +17,19 @@ export default function TodoList() {
       return prevTodos.filter((t) => t.id !== id);
     });
   };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => (
@@ -24,6 +37,7 @@ export default function TodoList() {
           todo={todo}
           key={todo.id}
           removeTodo={() => removeTodo(todo.id)}
+          toggle={() => toggleTodo(todo.id)}
         />
       ))}
     </List>
